0c1232e082a46ef4ca253acf9fc674adbdc59780
Pradeep Kumar <pk695283@gmail.com>
2023-12-23 23:20:59 +0530

    committed edit and delete functionality

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ const students = [
   }
 ]; 
 
+let editingStudent = null;
+
 function renderStudents(stu){
   const tbody = document.getElementById("tbody");
   tbody.innerHTML="";
@@ -33,14 +35,56 @@ function renderStudents(stu){
     for(let i=0;i<stu.length;i++){
       const student = stu[i];
       const row = document.createElement("tr");
-      row.innerHTML="<td>" + student.ID + "</td>" +
-      "<td>" + student.name + "</td>" +
-      "<td>" + student.email + "</td>" +
-      "<td>" + student.age + "</td>" +
-      "<td>" + student.gpa + "</td>" +
-      "<td>" + student.degree + "</td>";
+      row.innerHTML=`<td>  ${student.ID}  </td> 
+      <td>  ${student.name}  </td> 
+      <td>  ${student.email}  </td> 
+      <td>  ${student.age}  </td> 
+      <td>  ${student.gpa}  </td>
+      <td>  <div class="degree">
+              <span>${student.degree}</span>
+              <div>
+                <span><i class="icon fa-solid fa-pen-to-square"></i></span>
+                <span><i class="icon fa-solid fa-trash"></i></span>
+              </div> 
+            </div>
+          </td>`
       tbody.appendChild(row);
   }
+  
+  const editIcons = document.querySelectorAll('.fa-pen-to-square');
+    editIcons.forEach(icon => {
+        icon.addEventListener('click', function () {
+                const rowToEdit = this.closest('tr');
+                const studentID = parseInt(rowToEdit.querySelector('td:first-child').textContent);
+                const studentToEdit = students.find(student => student.ID === studentID);
+
+                if (studentToEdit) {
+                    // Set the currently editing student
+                    editingStudent = studentToEdit;
+
+                    // Populate input fields with the details of the selected student
+                    Name.value = studentToEdit.name;
+                    Email.value = studentToEdit.email;
+                    GPA.value = studentToEdit.gpa;
+                    AGE.value = studentToEdit.age;
+                    Degree.value = studentToEdit.degree;
+
+                    // Change button text to "Edit Student"
+                    document.getElementById('addStudent').style.display = "none";
+                    document.getElementById('edit').style.display = "block";
+                }
+      });
+  });
+
+
+  const deleteIcons = document.querySelectorAll('.fa-trash');
+        deleteIcons.forEach(icon => {
+            icon.addEventListener('click', function () {
+                const rowToDelete = this.closest('tr');
+                const studentID = parseInt(rowToDelete.querySelector('td:first-child').textContent);
+                deleteStudent(studentID);
+            });
+        });
   }
   else{
     const row = document.createElement("tr");
@@ -49,8 +93,36 @@ function renderStudents(stu){
   }
 
 }
-renderStudents(students);
+const editStudents=document.getElementById('edit');
+  editStudents.addEventListener("click",function(e){
+    e.preventDefault()
+    console.log("editStudent");
+    students.forEach(element => {
+      if(element.ID===editingStudent.ID) {
+       element.name=document.getElementById("name").value;
+       element.age=document.getElementById("age").value;
+       element.email=document.getElementById("email").value;
+       element.degree=document.getElementById("degree").value;
+       element.gpa=document.getElementById("gpa").value;
+  }});
+  renderStudents(students);
+  document.getElementById('addStudent').style.display = "block";
+  document.getElementById('edit').style.display = "none";
+  Name.value = ""; 
+    Email.value = "";
+    GPA.value = "";
+    AGE.value = "";
+    Degree.value = "";
+  });
 
+renderStudents(students);
+function updateTableRow(row, student) {
+  row.querySelector('td:nth-child(2)').textContent = student.name;
+  row.querySelector('td:nth-child(3)').textContent = student.email;
+  row.querySelector('td:nth-child(4)').textContent = student.age;
+  row.querySelector('td:nth-child(5)').textContent = student.gpa;
+  row.querySelector('td:nth-child(6) span').textContent = student.degree;
+}
 const itemForm = document.getElementById("itemForm");
 const Name = document.getElementById("name");
 const Email = document.getElementById("email");
@@ -60,17 +132,24 @@ const Degree = document.getElementById("degree");
 const itemList = document.getElementById("tbody");
 
 
-itemForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-
+document.getElementById('addStudent').addEventListener("click", function (event) {
+  console.log("added")
+  event.preventDefault();
     const GetName = Name.value;
     const GetEmail = Email.value;
     const GetGPA = GPA.value;
     const GetAGE = AGE.value;
     const GetDegree = Degree.value;
+    
+    if(GetName==="" || GetEmail==="" || GetDegree==="" || GetAGE==="" || GetGPA===""){
+      alert("All fields required!");
+      return
+    }
+
+    const newStudentID = calculateNewStudentID();
 
     const newStudent = {
-      ID:students.length+1,
+      ID:newStudentID,
       name:GetName,
       email:GetEmail,
       gpa:GetGPA,
@@ -86,6 +165,7 @@ itemForm.addEventListener("submit", function (event) {
     GPA.value = "";
     AGE.value = "";
     Degree.value = "";
+
 
 });
 
@@ -108,4 +188,28 @@ console.log("query",query);
       }
   }
   renderStudents(searchedStudents);
+}
+
+function calculateNewStudentID() {
+  const existingIDs = students
+        .map(student => student.ID)
+        .filter(id => typeof id === 'number' && !isNaN(id));
+
+    if (existingIDs.length === 0) {
+        // If no numeric IDs found, start with ID 1
+        return 1;
+    }
+
+    const maxID = Math.max(...existingIDs);
+    return maxID + 1;
+
+}
+
+function deleteStudent(studentID) {
+  const index = students.findIndex(student => student.ID === studentID);
+
+  if (index !== -1) {
+      students.splice(index, 1);
+      renderStudents(students);
+  }
 }
